7a9d5b9f55106492d3c516ae692b048084976be1
sl4vushka <romanov11899@gmail.com>
2022-12-20 22:48:24 +0200

    Create 9 laba.cpp

---
+++ 9 laba.cpp
@@ -0,0 +1,54 @@
+﻿#define _USE_MATH_DEFINES
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+class square
+{
+private:
+    double m_side;
+public:
+    square(double side)
+    {
+        m_side = side;
+    }
+    square()
+    {
+        m_side = 1;
+    }
+    square(square& A)
+    {
+        m_side = A.get_r();
+    }
+    ~square()
+    {
+
+    }
+
+    double get_r()
+    {
+        return m_side;
+    }
+    void set_r(double side)
+    {
+        m_side = side;
+    }
+
+    void show_r()
+    {
+        cout << m_side;
+    }
+
+    double Square_diagonal()
+    {
+        return (pow(m_side, 2));
+    }
+};
+
+int main()
+{
+    square one(10);
+    cout << one.Square_diagonal() << endl;
+
+}
+
